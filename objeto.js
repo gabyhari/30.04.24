12b507d37a587ba6326743b1ee4c91245f347f2b
gabyhari <142436298+gabyhari@users.noreply.github.com>
2024-04-30 11:33:15 -0300

    Add files via upload

---
+++ objeto.js
@@ -0,0 +1,19 @@
+    class Calebe {
+        constructor (pinterest1, pinterest2) {
+            this.pinterest = pinterest; //variavel de interface
+            this.pinterest2 = pinterest2; //variavel de interface
+            //açoes
+        }
+
+        metodo1 () {
+            //açoes
+             window.open(this.site);
+             window.open(this.pinterest1);
+        }
+
+        metodo2 () {
+            //açoes
+            window.open(this.site);
+            window.open(this.pinterest2);
+        }
+    }
